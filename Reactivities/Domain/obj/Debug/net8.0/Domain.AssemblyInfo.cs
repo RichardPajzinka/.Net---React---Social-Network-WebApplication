fad297903dd95956b166c9df5853a940e76d3e7d
RichardPajzinka <rpajzinka@gmail.com>
2024-06-08 19:17:58 +0200

    Mediator_change
    Mediator_change

--- Reactivities/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ Reactivities/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b71ffed676d4f400efe8702737ebd1787bc6e33d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03dfc084fa62f2f4e0c07bce5365b81cf60e0dc3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
